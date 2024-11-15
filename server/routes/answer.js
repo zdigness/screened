67dd6de3c7e7e186a2b6daa96d6720eb321b53be
Zoli Digness <zdigness@gmail.com>
2024-11-14 19:44:41 -0700

    update API endpoint for answer submission and fix date formatting in database query

--- server/routes/answer.js
+++ server/routes/answer.js
@@ -17,7 +17,7 @@ router.post('/submit-answer', async (req, res) => {
 		// Query today's movie from the database
 		const todayMovieResult = await db.query(
 			'SELECT * FROM movies WHERE game_date = $1',
-			moment().tz('America/Denver').format('YYYY-MM-DD')
+			[moment().tz('America/Denver').format('YYYY-MM-DD')]
 		);
 
 		if (movieResult.rows.length > 0) {
