d8a3c4ee942761ad457a0082828dd555dc196cf9
Zoli Digness <zdigness@gmail.com>
2024-11-14 18:48:09 -0700

    update database configuration for production environment

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,12 +2,18 @@
 require('dotenv').config(); // To load environment variables from .env
 const { Pool } = require('pg');
 
+// production
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
+	connectionString: process.env.DATABASE_URL,
 });
+
+// development
+// const pool = new Pool({
+// 	user: process.env.DB_USER,
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_NAME,
+// 	password: process.env.DB_PASS,
+// 	port: process.env.DB_PORT,
+// });
 
 module.exports = pool;
