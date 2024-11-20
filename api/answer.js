8e2e3233f7cb8e352cddadc6d24be2d70328caa8
Zoli Digness <zdigness@gmail.com>
2024-11-20 16:32:35 -0700

    feat: implement CORS middleware in API handlers for enhanced security and request handling

--- api/answer.js
+++ api/answer.js
@@ -1,4 +1,11 @@
 const { Pool } = require('pg');
+const cors = require('cors');
+
+// Enable CORS
+const corsMiddleware = cors({
+	origin: 'https://screened.vercel.app',
+	methods: ['POST'],
+});
 
 // Initialize database connection pool
 const pool = new Pool({
@@ -8,16 +15,13 @@ const pool = new Pool({
 
 // Serverless function handler
 export default async function handler(req, res) {
-	// Set CORS headers
-	res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (replace '*' with your frontend domain for better security)
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Specify allowed HTTP methods
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow necessary headers
+	// Apply CORS
+	corsMiddleware(req, res, () => {
+		if (req.method === 'OPTIONS') {
+			return res.status(200).end();
+		}
+	});
 
-	if (req.method === 'OPTIONS') {
-		// Handle CORS preflight requests
-		res.status(200).end();
-		return;
-	}
 	// Handle only POST requests
 	if (req.method !== 'POST') {
 		return res.status(405).json({ error: 'Method not allowed' });
--- api/movie.js
+++ api/movie.js
@@ -1,4 +1,11 @@
 const { Pool } = require('pg');
+const cors = require('cors');
+
+// Enable CORS
+const corsMiddleware = cors({
+	origin: 'https://screened.vercel.app',
+	methods: ['POST'],
+});
 
 // Initialize database connection pool
 const db = new Pool({
@@ -6,16 +13,13 @@ const db = new Pool({
 });
 
 export default async (req, res) => {
-	// Set CORS headers
-	res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (replace '*' with your frontend domain for better security)
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Specify allowed HTTP methods
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow necessary headers
+	// Apply CORS
+	corsMiddleware(req, res, () => {
+		if (req.method === 'OPTIONS') {
+			return res.status(200).end();
+		}
+	});
 
-	if (req.method === 'OPTIONS') {
-		// Handle CORS preflight requests
-		res.status(200).end();
-		return;
-	}
 	const { method, query } = req;
 
 	// Get MST timezone date
